09649e0e14af1733d729310304594e63dfe64c4b
Anthony Moss <asmoss93@gmail.com>
2019-05-06 10:01:23 -0400

    adds skeleton of notes application

---
+++ src/NotesApp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import NotesList from './NotesList';
+import NotesDetail from './NotesDetail';
+
+export default class NotesApp extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: []      // This will hold array of objects
+        }
+    }
+    render() {
+        return (
+            <div>
+                <NotesList />
+                <NotesDetail />
+            </div>
+        )
+    }
+}
+
+
